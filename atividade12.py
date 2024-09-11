ee36ca194f10caad4fb465e33745822568bd3fef
unknown <vitorlopes1248@gmail.comgit config --global user.name>
2024-09-11 15:24:29 -0300

    atividade12 nota do aluno

---
+++ atividade12.py
@@ -0,0 +1,9 @@
+# Crie um programa que receba a nota de um aluno e informe se ele foi aprovado ou reprovado. Considere que a média para aprovação é 7.
+
+nota = int(input("Digite sua nota: "))
+
+if (nota>=7):
+    print("parabens, você foi aprovado! ")
+
+else:
+    print("Você foi reprovado!")
